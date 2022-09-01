8f1e60bb075099b5a44e5002312cd5891b4d0dfc
solidoracle <solidoracle17@gmail.com>
2022-09-01 14:25:11 +0200

    infura set-up

---
+++ call.js
@@ -0,0 +1,26 @@
+const Web3 = require("web3");
+
+// Loading the Sarco Staking contract ABI
+const fs = require("fs");
+const abi  = JSON.parse(fs.readFileSync("sarcoStaking.json"));
+
+async function main() {
+  const network = process.env.ETHEREUM_NETWORK;
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider(
+      `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`
+    )
+  );
+  
+  const contract = new web3.eth.Contract(
+    abi,
+    process.env.CONTRACT_ADDRESS
+  );
+
+  const exampleFunction = contract.methods.totalStakers();
+  console.log(exampleFunction)
+
+}
+
+require("dotenv").config();
+main();
