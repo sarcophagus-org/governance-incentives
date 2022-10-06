2f05a8930d61212fd7354315629cec93d1f9b9c4
solidoracle <solidoracle17@gmail.com>
2022-10-06 11:47:32 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { fetchVoteData } from "./queries/voting-data";
+import { BigNumber, ethers } from 'ethers';
 const Web3 = require("web3");
 require("dotenv").config();
 const fs = require("fs");
+const zero = ethers.constants.Zero
 
 if(!process.env.VOTE_ID) {
   throw Error("Vote ID is required as an env variable")
@@ -11,7 +13,8 @@ if(!process.env.VOTE_ID) {
 const voteId = process.env.VOTE_ID
 
 // TODO: This will come from the collections contract
-const DISTRIBUTION_AMOUNT = 10000
+const DISTRIBUTION_AMOUNT = ethers.utils.parseEther('100')
+//ethers.utils.parseEther('100')
 
 const web3 = new Web3(
   new Web3.providers.HttpProvider(
@@ -29,24 +32,27 @@ async function main() {
   const voteData = await fetchVoteData(web3, voteId)
   const blockNumber = voteData.snapshotBlockNumber
  
-  const didVoteAddresses = new Map<string, number>()
-  let totalVoteBalance = 0
+  const didVoteAddresses = new Map<string, BigNumber>()
+  let totalVoteBalance: BigNumber = zero
   for (let i = 0; i < voteData.addresses.length; i++) {
     const votingAddress = voteData.addresses[i]
-    const stakedValueAt = await stakingContract.methods.stakeValueAt(votingAddress,blockNumber).call()
-    totalVoteBalance += +stakedValueAt
+    const stakedValueAt: BigNumber = await stakingContract.methods.stakeValueAt(votingAddress,blockNumber).call()
+    totalVoteBalance = totalVoteBalance.add(stakedValueAt)
     didVoteAddresses.set(votingAddress, stakedValueAt)
   }
   
-  const distributionMapping = new Map<string, number>();
+  console.log("didVoteAddresses:",didVoteAddresses)
+  console.log("totalVoteBalance:",totalVoteBalance.toString())
+
+  const distributionMapping = new Map<string, BigNumber>();
   for (let i = 0; i < voteData.addresses.length; i++) {
     const votingAddress = voteData.addresses[i]
-    const stakedValueAt = didVoteAddresses.get(votingAddress)
-    const percentage = (stakedValueAt / totalVoteBalance)
-    const distributionAmount = DISTRIBUTION_AMOUNT * percentage
+    const stakedValueAt = BigNumber.from(didVoteAddresses.get(votingAddress))
+    const percentage = stakedValueAt.mul(100000000000).div(totalVoteBalance)
+    const distributionAmount = DISTRIBUTION_AMOUNT.mul(percentage).div(100000000000)
     distributionMapping.set(votingAddress, distributionAmount)
   }
-  console.log(distributionMapping)
+  console.log("Distribution Mapping:",distributionMapping)
 }
 
 (async () => {
